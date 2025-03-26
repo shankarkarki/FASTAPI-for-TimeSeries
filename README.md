6339e4894275334a2f934e156cdb3f8bfa39c6ae
Your Name <you@example.com>
2025-03-26 12:40:31 +0545

    Foundational API setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Working with FASTAPI and TimeSeries DB
  Data pipeline with FASTAPI for time seriesdb
  # Docker
- - 'docker build -t analytics-api -f Dockerfile .'
--   docker run analytics-api
+ - 'docker build -t analytics-api:latest -f Dockerfile .'
+-   docker run analytics-api:latest
--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
 from typing import Union
 
 from fastapi import FastAPI
+from api.events import router as events_router
+
 
 app = FastAPI()
+app.include_router(events_router, prefix="/events")
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Shankar"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/healthz")
+def read_api_health():
+    return {"status": "ok"}
+
+
+    
